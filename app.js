7ab6d0c4427d5b7a81a0838cca97e9c888049eaa
Numinor <numinor93@mail.ru>
2019-08-18 18:46:50 +0300

    Added sessions

--- app.js
+++ app.js
@@ -3,6 +3,8 @@ const path = require("path");
 const express = require("express");
 const mongoose = require("mongoose");
 const bodyParser = require("body-parser");
+const session = require('express-session');
+const mongoDBstore = require('connect-mongodb-session')(session);
 
 //routes
 const mainRoutes = require("./routes/main");
@@ -14,11 +16,27 @@ const MONGODB_URI =
 
 //initialize app
 const app = express();
+const store = new mongoDBstore({
+  uri: MONGODB_URI,
+  collection: 'sessions'
+})
 
 app.set("view engine", "ejs");
 
 //parser BEFORE routes use
 app.use(bodyParser.urlencoded({ extended: false }));
+app.use(session({
+  secret: 'secret car',
+  resave: false,
+  saveUninitialized: false,
+  store: store
+}))
+
+app.use((req,res,next) => {
+  req.session.myValue = "myValue";
+  console.log(req.session);
+  next();
+})
 
 //routes use
 app.use(mainRoutes);
