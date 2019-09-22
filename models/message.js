d779ebf1830a9e4a0f0279b56519c05aa7afbef7
Numinor <numinor93@mail.ru>
2019-09-22 21:19:31 +0300

    Almost works in deployment

--- models/message.js
+++ models/message.js
@@ -10,10 +10,6 @@ const messageSchema = new Schema({
     nickname: {
         type: String,
         required: true
-    },
-    userId: {
-        type: String,
-        required: true
     }
 })
 
